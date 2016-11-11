4d5028fc443f3b436aa564b64b4d9b9ee4cf2fec
mm <l>
2016-11-11 14:45:44 +0800

    commit

--- js/common.js
+++ js/common.js
@@ -2,20 +2,19 @@
  * Created by Lysea on 2016/10/14.
 //  */
 
-// window.onload = function(){
-//
-//     var images = document.getElementsByClassName('banner_pic');
-//     var pos = 0;
-//     var len = images.length;
-//     setInterval(function(){
-//
-//         images[pos].style.display = 'none';
-//         pos = ++pos == len ? 0 : pos;
-//         images[pos].style.display = 'inline';
-//
-//     },2000);
-// };
-
+var i=0;
+$(function(){
+    $(".banner-pic").not(":eq(0)").hide();   //不是第一张图片的其余图片都隐藏
+    setInterval(function(){
+        i++;
+        if(i==2)
+        {
+            i=0;
+        }
+        $(".banner-pic").eq(i).show();           //第i个图片显示
+        $(".banner-pic").not(":eq("+i+")").hide();             //不是第i个图片隐藏
+    },2000);
+})
  function show() {
      // var sidebar = document.getElementsByClassName("sidebar");
      if (document.getElementsByClassName) {
