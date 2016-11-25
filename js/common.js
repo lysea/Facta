02b9376e71e362c35fc8670e357a721f5abfc41b
mm <l>
2016-11-25 11:24:36 +0800

    s

--- js/common.js
+++ js/common.js
@@ -3,12 +3,12 @@
 //  */
 
 var i=0;
-$(function(){
-    $(".banner-pic").not(":eq(0)").hide();   //不是第一张图片的其余图片都隐藏
-    setInterval(function(){
-        i++;
-        if(i==2)
-        {
+            $(function(){
+                $(".banner-pic").not(":eq(0)").hide();   //不是第一张图片的其余图片都隐藏
+                setInterval(function(){
+                    i++;
+                    if(i==2)
+                    {
             i=0;
         }
         $(".banner-pic").eq(i).show();           //第i个图片显示
@@ -19,21 +19,31 @@ $(function(){
      // var sidebar = document.getElementsByClassName("sidebar");
      if (document.getElementsByClassName) {
          var submenu = document.getElementsByClassName("sub-menu");
+         var open = document.getElementById("open");
          if (submenu[0].style.display == 'none') {
              submenu[0].style.display = 'block';
+             open.style.color="#0392ce";
          }
          else submenu[0].style.display = 'none';
+         open.style.color="#000";
      }
  }
 $(document).ready(function(){
-    $(".body-text").siblings("span").css("color","#0392ce");
-    $(".body-text").siblings(".text-button").css("background","url(style/images/btn_01.png) no-repeat");
-    $(".body-text").siblings(".text-button").click(function(){
-        $(".body-text").slideToggle("fast");
+    $(".row-blue").children("span").css("color","#0392ce");
+    $(".body-text").children(".off").css("background","url(style/images/btn_01.png) no-repeat");
+    $(".body-text").siblings(".on").css("background","url(style/images/btn_01.png) no-repeat");
+    $(".on").click(function(){
+        if($(".body-text").is(':visible')){
+            $(".body-text").hide();
+        }
+        $(this).siblings(".body-text").slideDown("fast");
+    });
+    $(".off").click(function(){
+        $(this).parent(".body-text").slideUp("fast");
     });
 });
 $(document).ready(function(){
-    $(".article").hover(function(){
+    $(".article a").hover(function(){
         $(this).children(".full-text").toggleClass("hover_bg")
     });
 });
