83021ec48d4f2fff1d9d93fdfc6b835416893c01
mm <l>
2016-12-06 17:39:09 +0800

    就是

--- js/common.js
+++ js/common.js
@@ -3,33 +3,35 @@
 //  */
 
 var i=0;
-            $(function(){
-                $(".banner-pic").not(":eq(0)").hide();   //不是第一张图片的其余图片都隐藏
-                setInterval(function(){
-                    i++;
-                    if(i==2)
-                    {
+$(function(){
+    $(".banner-pic").not(":eq(0)").hide();   //不是第一张图片的其余图片都隐藏
+    setInterval(function(){
+        i++;
+        if(i==2)
+        {
             i=0;
         }
         $(".banner-pic").eq(i).show();           //第i个图片显示
         $(".banner-pic").not(":eq("+i+")").hide();             //不是第i个图片隐藏
     },2000);
 })
- function show() {
-     if (document.getElementsByClassName) {
+window.onload=function () {
          var submenu = document.getElementsByClassName("sub-menu");
          var open = document.getElementById("open");
-         // submenu[0].style.display= 'none';
-         if (submenu[0].style.display == 'none') {
+         var on="none";
+         open.onclick=function () {
+         if (on == 'none') {
              submenu[0].style.display = 'block';
+             on="block";
              open.style.color="#0392ce";
          }
          else {
              submenu[0].style.display = 'none';
+             on="none";
              open.style.color="#000";
          }
      }
- }
+}
 $(document).ready(function(){
     $(".on").click(function(){
         $(".body-text").slideUp("fast");
